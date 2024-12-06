b6348b7cc5f45c00070ebf0c0fb63935a5aa0b81
Florussel F. Gallibot <jrgallibot@gmail.com>
2024-12-06 15:35:01 +0800

    Initial Commit

--- kt_auth/urls.py
+++ kt_auth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf import settings
-from kt_auth.pillars.views import PillarsView
+from kt_auth.pillars.views import pilars
 from kt_auth.signin.views import AuthSigninView, TemporarySigninView, signout
 from kt_auth.signup.views import AuthSignupView
 from kt_auth.reset_password.views import AuthResetPasswordView
@@ -9,7 +9,7 @@ from kt_auth.new_password.views import AuthNewPasswordView
 app_name = 'kt_auth'
 
 urlpatterns = [
-    path('signin', AuthSigninView.as_view(template_name = 'pages/auth/signin.html'), name='signin'),
+    path('signin', AuthSigninView.as_view(template_name = 'auth/signin.html'), name='signin'),
     path('signup', AuthSignupView.as_view(template_name = 'pages/auth/signup.html'), name='signup'),
     path('reset-password', AuthResetPasswordView.as_view(template_name = 'pages/auth/reset-password.html'), name='reset-password'),
     path('new-password', AuthNewPasswordView.as_view(template_name = 'pages/auth/new-password.html'), name='new-password'),
@@ -17,5 +17,5 @@ urlpatterns = [
     path('temporary/signin', TemporarySigninView.as_view(), name="temporary-signin"),
     path('signout', signout, name="signout"),
 
-    path('pillars', PillarsView.as_view(), name="pillars"),
+    path('pillars', pilars, name="pillars"),
 ]
