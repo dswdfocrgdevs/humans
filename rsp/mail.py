dfc30c9515539822268ff2043d41feb5bf13dd59
Jowam <69232719+dioame@users.noreply.github.com>
2025-01-28 12:16:50 +0800

    mailhog-config

--- rsp/mail.py
+++ rsp/mail.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
+import smtplib
 
 def send_email():
     email="dioamejade@gmail.com"
@@ -9,7 +10,7 @@ def send_email():
 
     subject = 'Caraga HUMANS TEST'
     content = 'TEST'
-    email_from = settings.EMAIL_HOST_USER
+    email_from = 'dioamejade@gmail.com'
 
     d = {
         'message': content,
@@ -25,3 +26,15 @@ def send_email():
 
     message.attach_alternative(html_body.render(d), 'text/html')
     message.send(fail_silently=False)
+
+
+# def send_email():
+#     # Connect to MailHog on port 1025 without STARTTLS
+#     server = smtplib.SMTP('localhost', 1025)
+
+#     # Login (if necessary, but MailHog usually doesn't require it)
+#     # server.login('your_username', 'your_password')
+
+#     # Send the email
+#     server.sendmail('from@example.com', 'dioamejade@gmail.com', 'Subject: Test\n\nThis is a test email.')
+#     server.quit()
